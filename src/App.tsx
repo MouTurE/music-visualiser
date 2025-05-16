d04a772fd9fabf515735324de808005f7bdc9f1c
MouTurE <m.turkalp53@gmail.com>
2025-05-16 08:27:31 +0300

    Deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { HashRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import Homepage from "./Pages/homepage";
-import Visualiser from "./Pages/visualiser";
+import { HashRouter as Router } from "react-router-dom";
 import AnimatedRoutes from "./Pages/Components/AnimatedRoutes";
 
-function App () {
-
-  
-
-  return <Router>
-   <AnimatedRoutes/>
-  </Router>;
+function App() {
+  return (
+    <Router>
+      <AnimatedRoutes />
+    </Router>
+  );
 }
 
 export default App;
